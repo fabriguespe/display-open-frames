6d06af495a178fc29b6d2a2c33544186a35b8a70
fabri <fguespe@gmail.com>
2024-03-16 11:56:07 -0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 // ./app/page.tsx
 "use client";
+import React, { useState } from "react"; // Import useState from React
 import { FrameUI, fallbackFrameContext } from "frames.js/render";
 import { signFrameAction, FarcasterSigner } from "frames.js/render/farcaster";
 import { FrameImageNext } from "frames.js/render/next";
 import { useFrame } from "frames.js/render/use-frame";
 
 export default function Page() {
+  const [homeframeUrl, setHomeframeUrl] = useState(
+    "https://a-frame-in-100-lines.vercel.app"
+  ); // State to hold the current homeframeUrl
+
+  const [inputUrl, setInputUrl] = useState(homeframeUrl); // State to hold the input value
   const farcasterSigner: FarcasterSigner = {
     fid: 1,
     status: "approved",
@@ -14,7 +20,7 @@ export default function Page() {
   };
 
   const frameState = useFrame({
-    homeframeUrl: "https://a-frame-in-100-lines.vercel.app",
+    homeframeUrl, // Use the state variable here
     frameActionProxy: "/frames",
     frameGetProxy: "/frames",
     frameContext: fallbackFrameContext,
@@ -30,8 +36,33 @@ export default function Page() {
     },
   });
 
+  // Function to handle the URL update
+  const updateUrl = () => {
+    setHomeframeUrl(inputUrl);
+  };
+
   return (
     <div className="w-[400px]" style={{ width: "400px" }}>
+      <div style={{ marginBottom: "0px" }}>
+        <input
+          type="text"
+          value={inputUrl}
+          onChange={(e) => setInputUrl(e.target.value)}
+          placeholder="Enter home frame URL"
+          style={{
+            marginRight: "10px",
+            width: "80%",
+            border: "0px",
+            color: "black",
+          }}
+        />
+        <button
+          onClick={updateUrl}
+          style={{ width: "13%", backgroundColor: "blue" }}
+        >
+          Fetch
+        </button>
+      </div>
       <FrameUI frameState={frameState} theme={{}} FrameImage={FrameImageNext} />
     </div>
   );
