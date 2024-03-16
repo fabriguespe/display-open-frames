92c552c318eeb3bfdd0afa3462f686bf7d43acb2
fabri <fguespe@gmail.com>
2024-03-16 11:20:55 -0300

    update

---
+++ src/app/frames/route.tsx
@@ -0,0 +1,2 @@
+// ./app/frames/route.tsx
+export { GET, POST } from "frames.js/render/next";
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,11 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+// tailwind.config.js
+const config = {
+  // ...
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/*.{ts,tsx,js,css}",
+    "./app/**/*.{ts,tsx,js,css}",
+    "./node_modules/frames.js/dist/render/next/*.{ts,tsx,js,css}",
+    "./node_modules/frames.js/dist/render/*.{ts,tsx,js,css}",
+    // ...
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
 };
-export default config;
